61203197b1681f39057e972e1a1f4d58b7e02a0e
hatchdreams <67721215+hatchdreams@users.noreply.github.com>
2022-01-16 17:10:56 -0500

    Component Split & Demo Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { StyleClassModule } from 'primeng/styleclass';
 import { RippleModule } from 'primeng/ripple';
 import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
 import { DialogModule } from 'primeng/dialog';
 import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { TabViewModule } from 'primeng/tabview';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule,
     StyleClassModule,
     RippleModule,
     ButtonModule,
     AvatarModule,
     DialogModule,
-    InputMaskModule
+    InputNumberModule,
+    InputMaskModule,
+    InputTextModule,
+    CheckboxModule,
+    RadioButtonModule,
+    DropdownModule,
+    InputTextareaModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
