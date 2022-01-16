b8d6c0e8830d72cb20a7609e408a59fb89f0f27a
hatchdreams <67721215+hatchdreams@users.noreply.github.com>
2022-01-15 23:51:40 -0500

    Initial Landing Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StyleClassModule } from 'primeng/styleclass';
+import { RippleModule } from 'primeng/ripple';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StyleClassModule,
+    RippleModule,
+    ButtonModule,
+    AvatarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
